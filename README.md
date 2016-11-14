dc003e72649ecced6732683b37c44cab37b10e20
Andy Gibb <aggibb@gmail.com>
2016-11-14 00:13:11 -0800

    Initial commits

---
+++ README.md
@@ -0,0 +1,52 @@
+ # geotagraw.pl
+
+## Synopsis
+A Perl script to copy geotags from JPGs to the corresponding raw file.
+
+## Usage
+
+`% geotagraw` will process files in the current and all subdirectories.
+
+`% geotagraw --nosubdirs` will only process files in the current directory.
+
+## Options
+
+* `-n`, `--nosubdirs`: do not process any subdirectories.
+
+* `-d`, `--debug`: print extra information (in practice, it only prints out the results of a calls to `Data::Dumper` at the moment).
+
+* `-q`,`--quiet`: suppress most text output.
+
+## Description
+
+The script looks for files with known raw format extensions and then in turn checks for correspondingly named JPGs. The `Image::ExifTool` module is used to read and copy all tags in the `gps` namespace from the JPG to the corresponding raw file.
+
+`geotagraw` will look for an equivalently named JPG first: e.g., for a file called `DSC05124.ARW` it will look for `DSC05124.JPG` first. Failing that, it will pick the first JPG that matches the filename. For example, I use DxO Optics Pro and processed JPGs have `_DxO9` appended to the filename. The script will look for those JPGs too and will check the first one it finds for geotags. (Of course, if that file does not contain any geotags but others do, then the program will never know about them.)
+
+By default, the script processes all subdirectories recursively, so be careful with the contents of those directories if you don't want the geotags transferred. Use the `-n` or `--nosubdirs` option to prevent this behaviour.
+
+## Notes
+
+Warning: This program **will** modify your files. Please make sure you have backups of your raw files before running this code. Existing geotags will be **overwritten** and you will not be able to get them back! But then you do have a backup, right?
+
+Currently only looks for Nikon `.NEF`, Sony `.ARW` and Canon `.CR2`  files.
+
+Error checking hasn't been tested yet.
+
+## Rationale
+
+Garmin Basecamp is great for viewing and editing my GPS tracks, and it's perfectly capable of using those tracks to geotag my photos. Alas, it only applies geotags to the JPGs, not the raw files...
+
+## Useful reading:
+
+I found the following webpages useful when writing `geotagraw`:
+* http://www.sno.phy.queensu.ca/~phil/exiftool/geotag.html
+* http://superuser.com/questions/377431/transfer-exif-gps-info-from-one-image-to-another
+* http://photo.stackexchange.com/questions/72711/how-can-i-copy-custom-exif-fields-from-one-image-to-another-with-exiftool
+* http://ninedegreesbelow.com/photography/exiftool-commands.html
+
+## License and copyright
+
+This software is licensed under GPL-3.0+.
+
+Copyright &copy; 2016 Andy Gibb
---
+++ geotagraw.pl
@@ -0,0 +1,143 @@
+# -*-perl-*-
+
+use strict;
+use warnings;
+use Cwd;
+use File::Basename;
+use File::Spec;
+use Image::ExifTool;
+use Data::Dumper;
+use Getopt::Long;
+
+my ($QUIET, $DEBUG, $NOSUBDIRS);
+my $status = GetOptions("quiet"  => \$QUIET,
+                        "debug" => \$DEBUG,
+			"nosubdirs" => \$NOSUBDIRS);
+
+# Make it so...
+process_dirs();
+
+############################
+sub process_dirs {
+  my $cwd = cwd();
+  print "Processing directory ".basename($cwd).": ";
+  opendir(my $DH, $cwd);
+  process_raw_files(get_raw_files($DH));
+
+  unless ($NOSUBDIRS) {
+    rewinddir($DH);
+    my @subdirs = grep {-d $_ && !/^\.{1,2}$/} readdir($DH);
+    if (@subdirs) {
+      my $nsubdirs = (@subdirs == 1) ? "1 subdirectory" : @subdirs." directories";
+      print "  Found $nsubdirs to process...\n" unless $QUIET;
+      print Dumper(\@subdirs) if $DEBUG;
+      foreach my $subdir (@subdirs) {
+	chdir(File::Spec->catdir($cwd, $subdir));
+	process_dirs();
+	chdir($cwd);
+      }
+    }
+  }
+}
+
+sub get_raw_files {
+  return map {$_} grep {/ARW$|NEF$|CR2$/i} readdir(shift);
+}
+
+sub process_raw_files {
+  if (@_) {
+    my $files = @_ == 1 ? "file" : "files";
+    print "found ".@_." raw $files to process\n";
+    print Dumper(\@_) if $DEBUG;
+    foreach my $rawfile (@_) {
+      print "  $rawfile: " unless $QUIET;
+      my $jpgfile = find_jpg_version($rawfile);
+      unless ($QUIET) {
+	if ($jpgfile) {
+	  print "found $jpgfile\n";
+	} else {
+	  print "has no corresponding jpg\n";
+	}
+      }
+      copy_geotags($jpgfile, $rawfile) if ($jpgfile);
+    }
+  } else {
+    print "no files to process\n";
+  }
+}
+
+sub find_jpg_version {
+  my $raw = shift;
+  my $cwd = cwd();
+  my ($rawname, $extension) = split(/\./, $raw);
+  opendir(my $DH, $cwd);
+  my @jpgs = map {$_} grep {/$rawname\S+jpg$/i} readdir($DH);
+  print Dumper(\@jpgs) if $DEBUG;
+  if (@jpgs) {
+    foreach my $jpg (@jpgs) {
+      return $jpg if ($jpg eq $rawname.".JPG");
+    }
+    return $jpgs[0];
+  }
+}
+
+sub copy_geotags {
+  my ($jpg, $raw) = @_;
+  my $cwd = cwd();
+  my $jpg_filename = File::Spec->catfile($cwd, $jpg);
+  my $raw_filename = File::Spec->catfile($cwd, $raw);
+  my $jpg_exif = Image::ExifTool->new();
+  my $raw_exif = Image::ExifTool->new();
+  my $jpg_geotags = $jpg_exif->ImageInfo($jpg_filename, 'gps:*');
+  if ($jpg_exif->GetValue('Error')) {
+    print $jpg_exif->GetValue('Error') ." - $jpg\n";
+    return;
+  }
+  my $raw_geotags = $raw_exif->ImageInfo($raw_filename, 'gps:*');
+  if ($raw_exif->GetValue('Error')) {
+    print $raw_exif->GetValue('Error') ." - $raw\n";
+    return;
+  }
+
+  if ($jpg_geotags && keys %$jpg_geotags > 1) {
+    my $tag_errors = 0;
+    foreach my $gpstag (keys %$jpg_geotags) {
+      # Catch the cases where the JPG and raw files have been geotagged separately
+      my $update_tag = ($raw_geotags->{$gpstag} &&
+			$raw_geotags->{$gpstag} ne $jpg_geotags->{$gpstag}) ? 1 : 0;
+      my $add_tag = ($raw_geotags->{$gpstag}) ? 0 : 1;
+      if ($add_tag || $update_tag) {
+	my ($success, $err) = $raw_exif->SetNewValue($gpstag, $jpg_geotags->{$gpstag});
+	if ($success) {
+	  print "    Setting $gpstag to " . $jpg_geotags->{$gpstag} . "\n" unless $QUIET;
+	} else {
+	  print "    Something went wrong setting $gpstag: " . $err ."\n" unless $QUIET;
+	  $tag_errors++;
+	}
+      } else {
+	print "    No need to update existing entry for $gpstag for $raw\n" unless $QUIET;
+      }
+    }
+    $raw_exif->WriteInfo($raw_filename) unless $tag_errors;
+  } else {
+    print "    No GPS tags in " . $jpg ."\n" unless $QUIET;
+  }
+}
+
+########
+
+=head1 NAME
+
+geotagraw.pl - a Perl script for copying existing EXIF geotags from JPGs to corresponding raw files
+
+=head1 DESCRIPTION
+
+See README.md for more info.
+
+=head1 AUTHOR
+
+Andy Gibb <aggibb@gmail.com>
+
+=head1 COPYRIGHT and LICENSE
+
+Copyright 2016 Andy Gibb.
